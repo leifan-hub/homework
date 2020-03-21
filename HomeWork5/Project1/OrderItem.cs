39f18ef09b308dcc7144f8637f6aa3dc6786ef79
雷凡 <2395171728@qq.com>
2020-03-21 10:10:11 +0800

    Signed-off-by: 雷凡 <2395171728@qq.com>

---
+++ HomeWork5/Project1/OrderItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    class OrderItem
+    {
+        internal Products Brand { get; set; }
+        public int ProductsNum { get; set; }
+        public double Price { get; set; }
+
+        public OrderItem(Products brand, int productsNum, double price)
+        {
+            Brand = brand;
+            ProductsNum = productsNum;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Brand + " " + ProductsNum + " " + Price;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is OrderItem item &&
+                   Brand == item.Brand &&
+                   ProductsNum == item.ProductsNum &&
+                   Price == item.Price;
+        }
+
+        public override int GetHashCode()
+        {
+            var hashCode = -510703613;
+            hashCode = hashCode * -1521134295 + Brand.GetHashCode();
+            hashCode = hashCode * -1521134295 + ProductsNum.GetHashCode();
+            hashCode = hashCode * -1521134295 + Price.GetHashCode();
+            return hashCode;
+        }
+
+    }
+}
---
+++ HomeWork5/Project1/OrderService.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork5
+{
+    class OrderService
+    {
+        public List<Order> Orders { set; get; }
+
+        public void AddOrder(Order order)
+        {
+            bool flag = false;
+            foreach (Order _order in Orders)
+            {
+                flag = _order.Equals(order);
+                if (flag)
+                    break;
+            }
+            if (flag == false)
+            {
+                Orders.Add(order);
+            }
+        }
+
+        public void RemoveOrder(Order order)
+        {
+            Orders.Remove(order);
+        }
+
+        public void RemoveAllOrders()
+        {
+            Orders.Clear();
+        } 
+
+        public Order GetOrderByOrderId(int id)
+        {
+            var selectedOrders = Orders.Where(order => order.OrderId == id);
+            return selectedOrders.First();
+        }
+
+        public IEnumerable<Order> FindOrderByClientName(string clientName)
+        {
+            var selectedOrders = Orders.Where(order => order.ClientName == clientName).OrderBy(order => order.OrderPrice);
+            return selectedOrders;
+        }
+
+        public IEnumerable<Order> FindOrderByProductsBrand(Products brand)
+        {
+            List<Order> selectedOrders = new List<Order>();
+            foreach (Order order in Orders)
+            {
+                var selectedOrderItems = order.OrderItems.Where(orderItem => orderItem.Brand == brand);
+                if (selectedOrderItems.First() != null)
+                {
+                    selectedOrders.Add(order);
+                }
+            }
+            return selectedOrders.OrderBy(order => order.OrderPrice);
+        }
+
+        public IEnumerable<Order> SortOrders(List<Order> orders)
+        {
+            return orders.OrderBy(order => order.OrderId);
+        }
+    }
+}
