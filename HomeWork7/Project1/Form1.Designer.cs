fe950cd5b27c305a4368165021251413961f6761
雷凡 <2395171728@qq.com>
2020-03-31 21:40:31 +0800

    Signed-off-by: 雷凡 <2395171728@qq.com>

---
+++ HomeWork7/Project1/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox_n = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_leng = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_th1 = new System.Windows.Forms.TextBox();
+            this.textBox_th2 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.trackBar_per1 = new System.Windows.Forms.TrackBar();
+            this.trackBar_per2 = new System.Windows.Forms.TrackBar();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_per1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_per2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(766, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Draw";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(652, 505);
+            this.panel1.TabIndex = 1;
+            // 
+            // textBox_n
+            // 
+            this.textBox_n.Location = new System.Drawing.Point(873, 111);
+            this.textBox_n.Name = "textBox_n";
+            this.textBox_n.Size = new System.Drawing.Size(100, 28);
+            this.textBox_n.TabIndex = 2;
+            this.textBox_n.Leave += new System.EventHandler(this.textBox_n_Leave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(703, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "递归深度";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(703, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 18);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "画笔颜色";
+            // 
+            // textBox_leng
+            // 
+            this.textBox_leng.Location = new System.Drawing.Point(873, 206);
+            this.textBox_leng.Name = "textBox_leng";
+            this.textBox_leng.Size = new System.Drawing.Size(100, 28);
+            this.textBox_leng.TabIndex = 6;
+            this.textBox_leng.Leave += new System.EventHandler(this.textBox_leng_Leave);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(703, 216);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 18);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "主干长度";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(703, 263);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 18);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "右分支长度比";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(703, 338);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 18);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "左分支长度比";
+            // 
+            // textBox_th1
+            // 
+            this.textBox_th1.Location = new System.Drawing.Point(873, 398);
+            this.textBox_th1.Name = "textBox_th1";
+            this.textBox_th1.Size = new System.Drawing.Size(100, 28);
+            this.textBox_th1.TabIndex = 12;
+            this.textBox_th1.Leave += new System.EventHandler(this.textBox_th1_Leave);
+            // 
+            // textBox_th2
+            // 
+            this.textBox_th2.Location = new System.Drawing.Point(873, 450);
+            this.textBox_th2.Name = "textBox_th2";
+            this.textBox_th2.Size = new System.Drawing.Size(100, 28);
+            this.textBox_th2.TabIndex = 13;
+            this.textBox_th2.Leave += new System.EventHandler(this.textBox_th2_Leave);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(703, 408);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(98, 18);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "右分支角度";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(703, 460);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(98, 18);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "左分支角度";
+            // 
+            // trackBar_per1
+            // 
+            this.trackBar_per1.Location = new System.Drawing.Point(854, 250);
+            this.trackBar_per1.Name = "trackBar_per1";
+            this.trackBar_per1.Size = new System.Drawing.Size(104, 69);
+            this.trackBar_per1.TabIndex = 16;
+            // 
+            // trackBar_per2
+            // 
+            this.trackBar_per2.Location = new System.Drawing.Point(854, 325);
+            this.trackBar_per2.Name = "trackBar_per2";
+            this.trackBar_per2.Size = new System.Drawing.Size(104, 69);
+            this.trackBar_per2.TabIndex = 17;
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.Color = System.Drawing.Color.White;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(873, 156);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 44);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Add Color";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(997, 505);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.trackBar_per2);
+            this.Controls.Add(this.trackBar_per1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox_th2);
+            this.Controls.Add(this.textBox_th1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_leng);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_n);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_per1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar_per2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox_n;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_leng;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_th1;
+        private System.Windows.Forms.TextBox textBox_th2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TrackBar trackBar_per1;
+        private System.Windows.Forms.TrackBar trackBar_per2;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ HomeWork7/Project1/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        private Graphics graphics;
+        int n = 10;
+        double leng = 100;
+        double th=-Math.PI / 2;
+        double th1 = 30 * Math.PI / 180;
+        double th2 = 20 * Math.PI / 180;
+        double per1 = 0.6;
+        double per2 = 0.7;
+        private Pen pen=new Pen(Color.Red, 1);
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetDefaultText();
+            SetDefaultTrackBar();
+        }
+
+        private void SetDefaultText()
+        {
+            textBox_n.Text = "10";
+            textBox_leng.Text = "100";
+            textBox_th1.Text = "30";
+            textBox_th2.Text = "20";
+        }
+
+        private void SetDefaultTrackBar()
+        {
+            trackBar_per1.Maximum = 10;
+            trackBar_per1.Minimum = 0;
+            trackBar_per1.Value = 6;
+            trackBar_per2.Maximum = 10;
+            trackBar_per2.Minimum = 0;
+            trackBar_per2.Value = 7;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(int.TryParse(textBox_n.Text, out n)==false)
+                n=10;
+            if(double.TryParse(textBox_leng.Text, out leng)==false)
+                leng=100;
+            per1 = trackBar_per1.Value/10.0;
+            per2 = trackBar_per2.Value/10.0;
+            if(double.TryParse(textBox_th1.Text, out double _th1))
+                _th1=30;
+            if(double.TryParse(textBox_th2.Text, out double _th2))
+                _th2=20;
+            th1 = _th1 * Math.PI / 180;
+            th2= _th2 * Math.PI / 180;
+            if (graphics == null) graphics = this.panel1.CreateGraphics();
+            DrawCayleyTree(n, 200, 310, leng, th);
+        }
+        
+        void DrawCayleyTree(int n,double x0,double y0,double leng,double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            DrawLine(x0, y0, x1, y1);
+            DrawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            DrawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+
+        void DrawLine(double x0,double y0,double x1,double y1)
+        {
+                graphics.DrawLine(pen, (int)x0, (int)y0, (int)x1, (int)y1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ColorDialog ColorForm = new ColorDialog();
+            if (ColorForm.ShowDialog() == DialogResult.OK)
+            {
+                 pen.Color=ColorForm.Color;
+            }
+        }
+
+        private void textBox_n_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox_n.Text))
+            {
+                textBox_n.Text = "10";
+            }
+        }
+
+        private void textBox_leng_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox_leng.Text))
+            {
+                textBox_n.Text = "100";
+            }
+        }
+
+        private void textBox_th1_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox_th1.Text))
+            {
+                textBox_n.Text = "30";
+            }
+        }
+
+        private void textBox_th2_Leave(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(textBox_th2.Text))
+            {
+                textBox_n.Text = "20";
+            }
+        }
+    }
+}
